a574f3d66e9c9a8579bca8f987c11b0d2edb2480
san G <sghundre1991@gmail.com>
2018-11-21 16:51:33 +0530

    11-21-2018 OOPS Practice

---
+++ OOPSProject/MethodTypes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSProject
+{
+    //Types Of Method
+    class MethodTypes
+    {
+        /// <summary>
+        /// Non-Value Returning Method Without parameter
+        /// </summary>
+        public void Test1()                          /////Static In Behaviour
+        {
+            string name = "Santosh";
+            Console.WriteLine("Non-Value Returning Method Without parameter" + " " + name.ToUpper());
+        }
+        /// <summary>
+        /// Non-Value Returning Method With parameter
+        /// </summary>
+        /// <param name="name"></param>
+        public void Test2(string name)              ////Dynamic In Behaviour
+        {
+            Console.WriteLine("Non-Value Returning Method with parameter" + " " + name.ToUpper());
+        }
+        /// <summary>
+        /// Value Returning Method Without parameter
+        /// </summary>
+        /// <returns></returns>
+        public string Test3()                      /////Static In Behaviour
+        {
+            string name = "Santosh";
+            return name.ToUpper();
+        }
+        /// <summary>
+        /// Value Returning Method With parameter
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string Test4(string name)           ////Dynamic In Behaviour
+        {
+            return name.ToUpper();
+        }
+    }
+}
---
+++ OOPSProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodTypes methodTypes = new MethodTypes();
+            methodTypes.Test1();
+            Console.WriteLine("Please Enter The Name");
+            string name = Console.ReadLine();
+            methodTypes.Test2(name);
+            name=methodTypes.Test3();
+            Console.WriteLine("The Length of the Given Name" + name + " " + "Is:" + " " + name.Length);
+            Console.WriteLine("Please Enter The Name");
+            name = Console.ReadLine();
+            name= methodTypes.Test4(name);
+            Console.WriteLine("The Lower Case Of Given Name " + name + " " + "Is:" + " " + name.ToLower());
+            Console.ReadLine();
+        }
+    }
+}
